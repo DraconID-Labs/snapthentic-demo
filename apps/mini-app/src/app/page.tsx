ccc0bb0444986d996d85936bc6df6609a4d362cc
Hubert Kotlinski <kotlinskihubert00@gmail.com>
2025-07-04 19:51:41 +0200

    drop miniapp dir

--- apps/mini-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "~/components/SignIn";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-y-3 p-24">
-      <SignIn />
-    </main>
-  );
-}
--- apps/website/.eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  root: true,
-  extends: "@snapthentic/eslint-config/next",
-};
-module.exports = config;
